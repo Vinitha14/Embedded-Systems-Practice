febb2f64ec12d5c0e54158657a4a34bed6d03d67
Vinitha Maheswaran <14vinitha@gmail.com>
2021-01-14 01:56:52 +0530

    Add files via upload

---
+++ LAB 8/Nokia5110_4C123/nokia5110uartmain.d
@@ -0,0 +1,8 @@
+.\nokia5110uartmain.o: Nokia5110UARTMain.c
+.\nokia5110uartmain.o: PLL.h
+.\nokia5110uartmain.o: tm4c123gh6pm.h
+.\nokia5110uartmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\nokia5110uartmain.o: Nokia5110.h
+.\nokia5110uartmain.o: PLL.h
+.\nokia5110uartmain.o: UART.h
+.\nokia5110uartmain.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
